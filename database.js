82b77741975bfdc4a78d5065752a15fb674caade
Simon Harris <36566034+simonharrisco@users.noreply.github.com>
2024-06-25 15:25:02 +0100

    feat: connectionpool testin

--- database.js
+++ database.js
@@ -4,21 +4,26 @@ require("dotenv").config();
 let connection;
 if (process.env.DB_HOST) {
   // Replace the values with your database connection details
-  connection = mysql.createPool({
-    connectionLimit: 20,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
+  try {
+    connection = mysql.createPool({
+      connectionLimit: 20,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    return;
+  }
 
-  connection.connect((error) => {
-    if (error) {
-      console.error("Error connecting to the database:", error);
-      return;
-    }
-    console.log("Connected to the database successfully.");
-  });
+  // connection.connect((error) => {
+  //   if (error) {
+  //     console.error("Error connecting to the database:", error);
+  //     return;
+  //   }
+  //   console.log("Connected to the database successfully.");
+  // });
 } else {
   connection = null;
   console.error(
